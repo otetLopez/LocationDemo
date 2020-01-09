b25aade0567d7a9c496ded87221a97b821b11559
otetLopez <otet.tud@gmail.com>
2020-01-09 16:54:59 -0500

    Add user location

--- LocationDemo/ViewController.swift
+++ LocationDemo/ViewController.swift
@@ -8,20 +8,91 @@
 
 import UIKit
 import CoreLocation
+import MapKit
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
 
     // Create a variable of CLLocationManager
     var locationManager = CLLocationManager()
     
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        // Make sure to add privacy in Info.plist
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        // Can be set on the MKMapView Properties
+        mapView.showsUserLocation = true
     }
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        // Retrive the user location
+        let userLocation : CLLocation = locations[0]
+        
+        // Set location latitude and longitude
+        let latitude = userLocation.coordinate.latitude
+        let longitude = userLocation.coordinate.longitude
+        
+        // Set location deltas
+        let latDelta : CLLocationDegrees = 0.05
+        let longDelta : CLLocationDegrees = 0.05
+        
+        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
+        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        // Print user location coordinates
+        print(userLocation)
+        
+        // Find the user location address using GeoCoder
+        CLGeocoder().reverseGeocodeLocation(userLocation) { (placemarks, error) in
+            if let error = error {
+                print(error)
+            } else {
+                if let placemark = placemarks?[0] {
+                    var subThoroufare = ""
+                    if placemark.subThoroughfare != nil {
+                        subThoroufare = placemark.subThoroughfare!
+                    }
+                    
+                    var thoroufare = ""
+                    if placemark.thoroughfare != nil {
+                        thoroufare = placemark.thoroughfare!
+                    }
+                    
+                    var subLocality = ""
+                    if placemark.subLocality != nil {
+                        subLocality = placemark.subLocality!
+                    }
+                    
+                    var subAdministrativeArea = ""
+                    if placemark.subAdministrativeArea != nil {
+                        subAdministrativeArea = placemark.subAdministrativeArea!
+                    }
+                    
+                    var postalCode = ""
+                    if placemark.postalCode != nil {
+                        postalCode = placemark.postalCode!
+                    }
+                    
+                    var country = ""
+                    if placemark.country != nil {
+                        country = placemark.country!
+                    }
+                    
+                    print("\(subThoroufare) \(thoroufare)\n\(subLocality) \(subAdministrativeArea)\n\(postalCode) \(country)")
+                }
+            }
+        }
+    }
 
 }
 
